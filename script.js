d16f7e049f52d82bd8279981f60040beb302fee0
ela.fhd <elahe.fahmideh@gmail.com>
2024-08-28 12:52:50 +0330

    feat(install prompt):add install baner for showing install prompt to user

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+let deferredPrompt;
+let installBtn = document.querySelector(".install-btn");
+let installBanerWrapper = document.querySelector(".install-baner-wrapper");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installBanerWrapper.style.display = "flex";
+  return false;
+});
+
+installBtn.addEventListener("click", () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choice) => {
+      if (choice.outcome === "dismissed") {
+      } else {
+        installBanerWrapper.style.display = "none";
+      }
+    });
+  }
+});
+
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
